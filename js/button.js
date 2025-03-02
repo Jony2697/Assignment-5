a62fcb16fd24929ad0ee99b23561e33a5372ae8c
Jony2697 <ijony2697@gmail.com>
2025-03-02 22:23:46 +0600

    button work added

--- js/button.js
+++ js/button.js
@@ -15,6 +15,9 @@ const firstBtn=document.getElementById('first-btn')
 
           const sum=totalToNum+1;
           document.getElementById('total-complete').innerText=sum;
+          if(sub1 ===0){
+            alert('Congrats!!! You have completed all the current task')
+        }
                       
         }
         else{
@@ -38,7 +41,13 @@ const secondBtn=document.getElementById('second-btn').
                 const sum2=totalToNum+1;             
                 document.getElementById('task-assign').innerText=sub2;
                 document.getElementById('total-complete').innerText=sum2;
+                if(sub2 ===0){
+                    alert('Congrats!!! You have completed all the current task')
+                }
                 
+            }
+            else{
+                console.log("Nothing executed");
             }
         }
     )
@@ -57,10 +66,94 @@ const thirdBtn=document.getElementById('third-btn')
                 const sum3=totalToNum+1;
                 document.getElementById('task-assign').innerText=sub3;
                 document.getElementById('total-complete').innerText=sum3;
+                if(sub3 ===0){
+                    alert('Congrats!!! You have completed all the current task')
+                }
+            }
+            else{
+                console.log("Nothing executed");
             }
 
     })
+const fourthBtn=document.getElementById('fourth-btn')
+        .addEventListener('click',
+        function(event){
+            const complete=event.target.disabled=true;
+            const taskAssign=document.getElementById('task-assign').innerText;
+            const  convertToNum=parseInt(taskAssign);
+            const totalComplete=document.getElementById('total-complete').innerText;
+            const totalToNum=parseInt(totalComplete);
+            if(complete===true){
+                alert('Board Update Successfully');
+                const sub4=convertToNum-1;
+                const sum4=totalToNum+1;
+                document.getElementById('task-assign').innerText=sub4;
+                document.getElementById('total-complete').innerText=sum4;
+                if(sub4 ===0){
+                    alert('Congrats!!! You have completed all the current task')
+                }
+            }
+            else{
+                console.log("Nothing executed");
+            }
 
+
+      })
+const fifthBtn=document.getElementById('fifth-btn')
+        .addEventListener('click',
+        function(event){
+            const complete=event.target.disabled=true;
+            const taskAssign=document.getElementById('task-assign').innerText;
+            const  convertToNum=parseInt(taskAssign);
+            const totalComplete=document.getElementById('total-complete').innerText;
+            const totalToNum=parseInt(totalComplete);
+            if(complete===true){
+                alert('Board Update Successfully');
+                const sub5=convertToNum-1;
+                const sum5=totalToNum+1;
+                document.getElementById('task-assign').innerText=sub5;
+                document.getElementById('total-complete').innerText=sum5;
+                if(sub5 ===0){
+                    alert('Congrats!!! You have completed all the current task')
+                }
+            }
+            else{
+                console.log("Nothing executed");
+            }
+
+
+      })
+const sixBtn=document.getElementById('six-btn')
+        .addEventListener('click',
+        function(event){
+            const complete=event.target.disabled=true;
+            const taskAssign=document.getElementById('task-assign').innerText;
+            const  convertToNum=parseInt(taskAssign);
+            const totalComplete=document.getElementById('total-complete').innerText;
+            const totalToNum=parseInt(totalComplete);
+            if(complete===true){
+                alert('Board Update Successfully');            
+                const sub6=convertToNum-1;
+                const sum6=totalToNum+1;
+                document.getElementById('task-assign').innerText=sub6;
+                document.getElementById('total-complete').innerText=sum6;
+                if(sub6 ===0){
+                    alert('Congrats!!! You have completed all the current task')
+                }
+            }
+            else{
+                console.log("Nothing executed");
+            }
+
+
+      })
+
+
+
+ 
+ 
+ 
+    
 
 
 
