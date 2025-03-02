a6b1dee44cad3a43fa4beb4dc95a650aa0f5a197
Jony2697 <ijony2697@gmail.com>
2025-03-02 20:53:20 +0600

    js file added

---
+++ js/button.js
@@ -0,0 +1,47 @@
+
+const firstBtn=document.getElementById('first-btn')
+    .addEventListener('click',
+    function(event){
+       const complete= event.target.disabled=true;
+       const taskAssign=document.getElementById('task-assign').innerText;
+       const  convertToNum=parseInt(taskAssign);
+       const totalComplete=document.getElementById('total-complete').innerText;
+       const totalToNum=parseInt(totalComplete);
+        if(complete === true){
+            alert('Board Update Successfully')
+          const sub=convertToNum-1;
+          console.log(sub);
+          document.getElementById('task-assign').innerText=sub;
+
+          const sum=totalToNum+1;
+          document.getElementById('total-complete').innerText=sum;
+                      
+        }
+        else{
+            console.log("Nothing executed");            
+        }        
+    }
+)
+
+
+const secondBtn=document.getElementById('second-btn').
+    addEventListener('click',
+        function(event){
+            const complete=event.target.disabled=true;
+            const taskAssign=document.getElementById('task-assign').innerText;
+            const  convertToNum=parseInt(taskAssign);
+            const totalComplete=document.getElementById('total-complete').innerText;
+            const totalToNum=parseInt(totalComplete);
+            if(complete===true){
+                const sub2=convertToNum-1;
+                const sum2=totalToNum+1;             
+                document.getElementById('task-assign').innerText=sub2;
+                document.getElementById('total-complete').innerText=sum2;
+                
+            }
+        }
+    )
+
+
+
+
